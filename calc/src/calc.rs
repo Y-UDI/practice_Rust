8c253936792bbf16ae2e2ea32ac4fe1d3c6a3341
Y-UDI <72498597+Y-UDI@users.noreply.github.com>
2026-04-07 13:43:11 +0000

    calc

---
+++ calc/src/calc.rs
@@ -0,0 +1,16 @@
+use std::io;
+
+fn main() {
+    let sum = 5 + 10; // 15
+    println!("{}", sum);
+    let diff = 95.5 - 4.3; //91.2
+    println!("{}", diff);
+    let product = 4 * 30; // 120
+    println!("{}", product);
+    let quotient = 40.8 / 20.3; //2.0098522167487682
+    println!("{}", quotient);
+    let truncated = -5 / 3; //-1
+    println!("{}", truncated);
+    let remainder = 5 % 3;
+    println!("{}", remainder) // 2
+}
